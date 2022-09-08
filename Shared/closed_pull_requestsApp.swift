1fbf83a9b76d98fb9cd5540bf26fd8631690186a
divyakumarprajapati <divyakumarprajapati1@gmail.com>
2022-09-08 22:06:12 +0530

    Initial Commit

---
+++ Shared/closed_pull_requestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  closed_pull_requestsApp.swift
+//  Shared
+//
+//  Created by Divyakumar Prajapati on 08/09/22.
+//
+
+import SwiftUI
+
+@main
+struct closed_pull_requestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
